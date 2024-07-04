2bd4c5c3ac74864934b2bc47b295b4d6902df479
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-04 23:04:03 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -104,7 +104,9 @@ class Main extends Sprite
 			fpsVar.visible = ClientPrefs.showFPS;
 
 		Generic.mode = ROOTDATA;
-		
+		if (!FileSystem.exists(Generic.returnPath() + 'assets')) {
+			FileSystem.createDirectory(Generic.returnPath() + 'assets');
+		}
 		if (!FileSystem.exists(Generic.returnPath() + 'assets/videos')) {
 			FileSystem.createDirectory(Generic.returnPath() + 'assets/videos');
 		}
